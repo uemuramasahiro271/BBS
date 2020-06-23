f010699017c8e6814feabe0b319e2ce17d262ec8
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-06-23 22:27:08 +0900

    実装

--- src/main/java/com/bbs/app/BbsApplication.java
+++ src/main/java/com/bbs/app/BbsApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.bbs.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bbs.common")
 public class BbsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bbs/app/controller/top/TopController.java
+++ src/main/java/com/bbs/app/controller/top/TopController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.bbs.app.controller.top;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HeloController {
+public class TopController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
