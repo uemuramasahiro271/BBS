2ca3bec18babb0ebf59b1c7a7a3f595b61ec38bc
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-06-25 00:23:13 +0900

    実装

--- src/test/java/com/bbs/app/BbsApplicationTests.java
+++ src/test/java/com/bbs/app/BbsApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.bbs.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BbsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
