28b8673f549c5cabc7fe77b0ca4ced672289493a
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-07-11 00:34:29 +0900

    実装

--- src/main/java/com/bbs/app/postpage/service/PostPageService.java
+++ src/main/java/com/bbs/app/postpage/service/PostPageService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bbs.app.postpage.entity.ContentEntity;
 import com.bbs.app.postpage.entity.ContentPk;
 import com.bbs.app.postpage.repository.PostPageRepository;
+import com.bbs.app.selection_bbs.repository.BbsRepository;
 
 @Service
 public class PostPageService {
@@ -14,17 +15,24 @@ public class PostPageService {
 	@Autowired
 	private PostPageRepository postPageRepository;
 
+	@Autowired
+	private BbsRepository bbsRepository;
+
 	@Transactional
 	public void add(ContentEntity entity) {
-		postPageRepository.saveAndFlush(entity);
+		var bbsEntity = bbsRepository.findById(entity.getContentPk().getBbsId()).get();
+		bbsEntity.getContents().add(entity);
 	}
 
 	@Transactional
 	public void delete(int id, int no) {
+
+		var bbsEntity = bbsRepository.findById(id).get();
+
 		var pk = new ContentPk(id, no);
 		var entity = postPageRepository.findById(pk).get();
-		postPageRepository.delete(entity);
-		//postPageRepository.deleteById(new ContentPk(id, no));
+
+		bbsEntity.getContents().remove(entity);
 	}
 
 	@Transactional
