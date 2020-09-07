4e91cbb8171a338107ddef2d620fb7f33cbc09ba
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-09-07 22:31:48 +0900

    実装

--- src/main/java/com/bbs/app/authentication/service/AccountServiceImpl.java
+++ src/main/java/com/bbs/app/authentication/service/AccountServiceImpl.java
@@ -1,62 +1,71 @@
-//package com.bbs.app.authentication.service;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.data.domain.Sort.Direction;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.bbs.app.authentication.entity.Account;
-//import com.bbs.app.authentication.repository.AccountRepository;
-//
-//
-//@Service
-//public class AccountServiceImpl {
-//
-//	@Autowired
-//	private AccountRepository accountRepository;
-//
-//	@Autowired
-//	private BCryptPasswordEncoder passwordEncoder;
-//
-//	@Transactional(readOnly = true)
-//	public Account getAccount(String userName) {
-//		Account account = accountRepository.findOne(userName);
-//		if(account == null) {
-//			return null;
-//		}
-//		return account;
-//	}
-//
-//	@Transactional(readOnly = true)
-//	public List<Account> getAccountAll() {
-//		return accountRepository.findAll(new  Sort(Direction.ASC, "userName"));
-//	}
-//
-//	@Transactional
-//	public Account createAccount(String userName, String password) {
-//		Account account = new Account();
-//		account.setUserName(userName);
-//		account.setPassword(encode(password));
-//		return accountRepository.save(account);
-//	}
-//
-//	@Transactional
-//	public Account updateAccountName(String userName, String password) {
-//		Account account = getAccount(userName);
-//		account.setPassword(encode(password));
-//		return account;
-//	}
-//
-//	@Transactional
-//	public void deleteAccount(String userName) {
-//		accountRepository.delete(userName);
-//	}
-//
-//	private String encode(String password) {
-//		return passwordEncoder.encode(password);
-//	}
-//}
+package com.bbs.app.authentication.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bbs.app.entity.UserAccount;
+import com.bbs.app.entity.UserAccountExample;
+import com.bbs.app.mapper.UserAccountMapper;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+	@Autowired
+	private UserAccountMapper userAccountMapper;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserAccount getAccount(String userName) {
+		return userAccountMapper.selectByPrimaryKey(userName);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<UserAccount> getAccountAll() {
+
+		UserAccountExample example = new UserAccountExample();
+		example.setOrderByClause("name ASC");
+
+		return userAccountMapper.selectByExample(example);
+	}
+
+	@Override
+	@Transactional
+	public UserAccount createAccount(String userName, String password, String role) {
+		UserAccount account = new UserAccount();
+		account.setName(userName);
+		account.setPassword(encode(password));
+		account.setRole(role);
+
+		userAccountMapper.insert(account);
+		return account;
+	}
+
+	@Override
+	@Transactional
+	public UserAccount updateAccountName(String userName, String password) {
+		UserAccount account = getAccount(userName);
+		account.setPassword(encode(password));
+
+		userAccountMapper.updateByPrimaryKey(account);
+
+		return account;
+	}
+
+	@Override
+	@Transactional
+	public void deleteAccount(String userName) {
+		userAccountMapper.deleteByPrimaryKey(userName);
+	}
+
+	private String encode(String password) {
+		return passwordEncoder.encode(password);
+	}
+}
